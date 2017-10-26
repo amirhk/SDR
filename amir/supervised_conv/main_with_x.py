7fc9e697ccb879a114e1a9a69d8a1843dd17d325
Amir <amirhkarimi@gmail.com>
2017-10-25 22:20:20 -0400

    minor update

--- amir/supervised_conv/main_with_x.py
+++ amir/supervised_conv/main_with_x.py
@@ -31,8 +31,8 @@ from datetime import datetime
 #                                                                    Fetch Data
 # -----------------------------------------------------------------------------
 
-fh_import_dataset = lambda : importMnist()
-# fh_import_dataset = lambda : importMnistFashion()
+# fh_import_dataset = lambda : importMnist()
+fh_import_dataset = lambda : importMnistFashion()
 
 (dataset_name,
   x_train,
@@ -56,7 +56,7 @@ y_test = y_test[:training_size,:]
 
 batch_size = 100
 latent_dim = 15
-epochs = 50
+epochs = 300
 intermediate_dim = 500
 epsilon_std = 1.0
 learning_rate = 0.00001
@@ -99,16 +99,16 @@ def sampling(args):
                               stddev=epsilon_std)
     return z_mean + K.exp(z_log_var / 2) * epsilon
 
-# h_d_x_1 = Dense(4*4*8, activation = 'relu')
-# h_d_x_2 = Reshape((4,4,8))
-# h_d_x_3 = Conv2D(8, (3, 3), activation='relu', padding='same')
-# h_d_x_4 = UpSampling2D((2, 2))
-# h_d_x_5 = Conv2D(16, (3, 3), activation='relu', padding='same')
-# h_d_x_6 = UpSampling2D((2, 2))
-# h_d_x_7 = Conv2D(16, (3, 3), activation='relu')
-# h_d_x_8 = UpSampling2D((2, 2))
-# x_decoded_reshaped = Conv2D(1, (3, 3), activation='sigmoid', padding='same')
-# x_decoded = Flatten()
+h_d_x_1 = Dense(4*4*8, activation = 'relu')
+h_d_x_2 = Reshape((4,4,8))
+h_d_x_3 = Conv2D(8, (3, 3), activation='relu', padding='same')
+h_d_x_4 = UpSampling2D((2, 2))
+h_d_x_5 = Conv2D(16, (3, 3), activation='relu', padding='same')
+h_d_x_6 = UpSampling2D((2, 2))
+h_d_x_7 = Conv2D(16, (3, 3), activation='relu')
+h_d_x_8 = UpSampling2D((2, 2))
+x_decoded_reshaped = Conv2D(1, (3, 3), activation='sigmoid', padding='same')
+x_decoded = Flatten()
 
 h_d_y_1 = Dense(intermediate_dim, activation='relu')
 h_d_y_2 = Dense(intermediate_dim, activation='relu')
@@ -130,16 +130,16 @@ _z_mean = z_mean(_h_e_7)
 _z_log_var = z_log_var(_h_e_7)
 z = Lambda(sampling, output_shape=(latent_dim,))([_z_mean, _z_log_var])
 
-# _h_d_x_1 = h_d_x_1(z)
-# _h_d_x_2 = h_d_x_2(_h_d_x_1)
-# _h_d_x_3 = h_d_x_3(_h_d_x_2)
-# _h_d_x_4 = h_d_x_4(_h_d_x_3)
-# _h_d_x_5 = h_d_x_5(_h_d_x_4)
-# _h_d_x_6 = h_d_x_6(_h_d_x_5)
-# _h_d_x_7 = h_d_x_7(_h_d_x_6)
-# _h_d_x_8 = h_d_x_8(_h_d_x_7)
-# _x_decoded_reshaped = x_decoded_reshaped(_h_d_x_8)
-# _x_decoded = x_decoded(_x_decoded_reshaped)
+_h_d_x_1 = h_d_x_1(z)
+_h_d_x_2 = h_d_x_2(_h_d_x_1)
+_h_d_x_3 = h_d_x_3(_h_d_x_2)
+_h_d_x_4 = h_d_x_4(_h_d_x_3)
+_h_d_x_5 = h_d_x_5(_h_d_x_4)
+_h_d_x_6 = h_d_x_6(_h_d_x_5)
+_h_d_x_7 = h_d_x_7(_h_d_x_6)
+_h_d_x_8 = h_d_x_8(_h_d_x_7)
+_x_decoded_reshaped = x_decoded_reshaped(_h_d_x_8)
+_x_decoded = x_decoded(_x_decoded_reshaped)
 
 
 _h_d_y_1 = h_d_y_1(z)
@@ -148,19 +148,16 @@ _y_decoded = y_decoded(_h_d_y_2)
 
 ###### Define Loss ################################################################################
 
-# def vae_loss(x, _x_decoded):
-def vae_loss(y, _y_decoded):
+def vae_loss(x, _x_decoded):
 
-    # xent_loss = original_dim * objectives.binary_crossentropy(x, _x_decoded)
+    xent_loss = original_dim * objectives.binary_crossentropy(x, _x_decoded)
     kl_loss = - 0.5 * K.sum(1 + _z_log_var - K.square(_z_mean) - K.exp(_z_log_var), axis=-1)
     y_loss= 10 * objectives.categorical_crossentropy(yy, _y_decoded)
-    # return xent_loss + kl_loss + y_loss
-    return kl_loss + y_loss
+    return xent_loss + kl_loss + y_loss
 
 my_adam = optimizers.Adam(lr=learning_rate, beta_1=0.1)
 
-# vae = Model(inputs = [x,yy], outputs =[_x_decoded,_y_decoded]) #(x,_x_decoded)
-vae = Model(inputs = [x,yy], outputs =[_y_decoded]) #(x,_x_decoded)
+vae = Model(inputs = [x,yy], outputs =[_x_decoded,_y_decoded]) #(x,_x_decoded)
 vae.compile(optimizer=my_adam, loss=vae_loss)
 
 
@@ -181,28 +178,26 @@ _h_e_7_ = h_e_7(_h_e_6_)
 _z_mean_ = z_mean(_h_e_7_)
 
 
-# _h_d_x_1_ = h_d_x_1(_z_mean_)
-# _h_d_x_2_ = h_d_x_2(_h_d_x_1_)
-# _h_d_x_3_ = h_d_x_3(_h_d_x_2_)
-# _h_d_x_4_ = h_d_x_4(_h_d_x_3_)
-# _h_d_x_5_ = h_d_x_5(_h_d_x_4_)
-# _h_d_x_6_ = h_d_x_6(_h_d_x_5_)
-# _h_d_x_7_ = h_d_x_7(_h_d_x_6_)
-# _h_d_x_8_ = h_d_x_8(_h_d_x_7_)
-# _x_decoded_reshaped_ = x_decoded_reshaped(_h_d_x_8_)
-# _x_decoded_ = x_decoded(_x_decoded_reshaped_)
+_h_d_x_1_ = h_d_x_1(_z_mean_)
+_h_d_x_2_ = h_d_x_2(_h_d_x_1_)
+_h_d_x_3_ = h_d_x_3(_h_d_x_2_)
+_h_d_x_4_ = h_d_x_4(_h_d_x_3_)
+_h_d_x_5_ = h_d_x_5(_h_d_x_4_)
+_h_d_x_6_ = h_d_x_6(_h_d_x_5_)
+_h_d_x_7_ = h_d_x_7(_h_d_x_6_)
+_h_d_x_8_ = h_d_x_8(_h_d_x_7_)
+_x_decoded_reshaped_ = x_decoded_reshaped(_h_d_x_8_)
+_x_decoded_ = x_decoded(_x_decoded_reshaped_)
 
 _h_d_y_1_ = h_d_y_1(_z_mean_)
 _h_d_y_2_ = h_d_y_2(_h_d_y_1_)
 _y_decoded_ = y_decoded(_h_d_y_2_)
 
 
-# vaeencoder = Model(x,[_x_decoded_,_y_decoded_])
-vaeencoder = Model(x,[_y_decoded_])
+vaeencoder = Model(x,[_x_decoded_,_y_decoded_])
 #####################################################################################
 
-# _, b  = vaeencoder.predict(x_test, batch_size = batch_size)
-b  = vaeencoder.predict(x_test, batch_size = batch_size)
+_, b  = vaeencoder.predict(x_test,batch_size = batch_size)
 
 y_test_label = np.argmax(y_test,axis =1)
 
@@ -214,8 +209,7 @@ class ACCURACY(Callback):
 
     def on_epoch_end(self,batch,logs = {}):
         ii= pickle.load(open('counter', 'rb'))
-        # _, b  = vaeencoder.predict(x_test, batch_size = batch_size)
-        b  = vaeencoder.predict(x_test, batch_size = batch_size)
+        _, b  = vaeencoder.predict(x_test, batch_size = batch_size)
         Accuracy[ii, 0]
 
         lll = np.argmax(b, axis =1)
@@ -233,13 +227,11 @@ accuracy = ACCURACY()
 # model_weights = pickle.load(open('weights_vaesdr_' + str(latent_dim) + 'd_trained_on_' + dataset_name, 'rb'))
 # vae.set_weights(model_weights)
 
-# vae.fit([x_train, y_train],[x_train, y_train],
-vae.fit([x_train, y_train],[y_train],
+vae.fit([x_train, y_train],[x_train,y_train],
         shuffle=True,
         epochs=epochs,
         batch_size=batch_size,
-        # validation_data =([x_val,y_val],[x_val,y_val]),
-        validation_data =([x_val,y_val],[y_val]),
+        validation_data =([x_val,y_val],[x_val,y_val]),
         callbacks = [accuracy])
 
 model_weights = vae.get_weights()
@@ -258,86 +250,86 @@ fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], linewidth = 0, c=y_test_label)
 
-# #### build generator #########################################################################
-# generator_input = Input(shape=(latent_dim,))
+#### build generator #########################################################################
+generator_input = Input(shape=(latent_dim,))
 
-# _h_g_x_1_ = h_d_x_1(generator_input)
-# _h_g_x_2_ = h_d_x_2(_h_g_x_1_)
-# _h_g_x_3_ = h_d_x_3(_h_g_x_2_)
-# _h_g_x_4_ = h_d_x_4(_h_g_x_3_)
-# _h_g_x_5_ = h_d_x_5(_h_g_x_4_)
-# _h_g_x_6_ = h_d_x_6(_h_g_x_5_)
-# _h_g_x_7_ = h_d_x_7(_h_g_x_6_)
-# _h_g_x_8_ = h_d_x_8(_h_g_x_7_)
-# _x_generated_reshaped = x_decoded_reshaped(_h_g_x_8_)
-# _x_generated_ = x_decoded(_x_generated_reshaped)
+_h_g_x_1_ = h_d_x_1(generator_input)
+_h_g_x_2_ = h_d_x_2(_h_g_x_1_)
+_h_g_x_3_ = h_d_x_3(_h_g_x_2_)
+_h_g_x_4_ = h_d_x_4(_h_g_x_3_)
+_h_g_x_5_ = h_d_x_5(_h_g_x_4_)
+_h_g_x_6_ = h_d_x_6(_h_g_x_5_)
+_h_g_x_7_ = h_d_x_7(_h_g_x_6_)
+_h_g_x_8_ = h_d_x_8(_h_g_x_7_)
+_x_generated_reshaped = x_decoded_reshaped(_h_g_x_8_)
+_x_generated_ = x_decoded(_x_generated_reshaped)
 
-# generator = Model(generator_input,_x_generated_)
-#                                         # -------------------------------------
-#                                         #                               Fit GMM
-#                                         # -------------------------------------
+generator = Model(generator_input,_x_generated_)
+                                        # -------------------------------------
+                                        #                               Fit GMM
+                                        # -------------------------------------
 
-# # display a 2D plot of the digit classes in the latent space
-# x_train_encoded = encoder.predict(x_train, batch_size=batch_size)
+# display a 2D plot of the digit classes in the latent space
+x_train_encoded = encoder.predict(x_train, batch_size=batch_size)
 
-# n_components = num_classes
-# cv_type = 'full'
-# gmm = mixture.GaussianMixture(n_components=n_components, covariance_type=cv_type)
-# gmm.fit(x_train_encoded)
+n_components = num_classes
+cv_type = 'full'
+gmm = mixture.GaussianMixture(n_components=n_components, covariance_type=cv_type)
+gmm.fit(x_train_encoded)
 
-# x_decoded, b  = vaeencoder.predict(x_test,batch_size = batch_size)
-
-
-#                                         # -------------------------------------
-#                                         #                                 Plots
-#                                         # -------------------------------------
+x_decoded, b  = vaeencoder.predict(x_test,batch_size = batch_size)
 
 
-
-# def getFigureOfSamplesForInput(x_samples, sample_dim, number_of_sample_images, grid_x, grid_y):
-#     figure = np.zeros((sample_dim * number_of_sample_images, sample_dim * number_of_sample_images))
-#     for i, yi in enumerate(grid_x):
-#         for j, xi in enumerate(grid_y):
-#             digit = x_samples[i * number_of_sample_images + j, :].reshape(sample_dim, sample_dim)
-#             figure[i * sample_dim: (i + 1) * sample_dim,
-#                    j * sample_dim: (j + 1) * sample_dim] = digit
-#     return figure
+                                        # -------------------------------------
+                                        #                                 Plots
+                                        # -------------------------------------
 
 
-# number_of_sample_images = 15
-# grid_x = norm.ppf(np.linspace(0.05, 0.95, number_of_sample_images))
-# grid_y = norm.ppf(np.linspace(0.05, 0.95, number_of_sample_images))
 
-# plt.figure()
+def getFigureOfSamplesForInput(x_samples, sample_dim, number_of_sample_images, grid_x, grid_y):
+    figure = np.zeros((sample_dim * number_of_sample_images, sample_dim * number_of_sample_images))
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            digit = x_samples[i * number_of_sample_images + j, :].reshape(sample_dim, sample_dim)
+            figure[i * sample_dim: (i + 1) * sample_dim,
+                   j * sample_dim: (j + 1) * sample_dim] = digit
+    return figure
 
-# ax = plt.subplot(1,3,1)
-# x_samples_a = x_test
-# canvas = getFigureOfSamplesForInput(x_samples_a, sample_dim, number_of_sample_images, grid_x, grid_y)
-# plt.imshow(canvas, cmap='Greys_r')
-# ax.set_title('Original Test Images', fontsize=8)
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
 
-# ax = plt.subplot(1,3,2)
-# x_samples_b = x_decoded
-# canvas = getFigureOfSamplesForInput(x_samples_b, sample_dim, number_of_sample_images, grid_x, grid_y)
-# plt.imshow(canvas, cmap='Greys_r')
-# ax.set_title('Reconstructed Test Images', fontsize=8)
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
+number_of_sample_images = 15
+grid_x = norm.ppf(np.linspace(0.05, 0.95, number_of_sample_images))
+grid_y = norm.ppf(np.linspace(0.05, 0.95, number_of_sample_images))
 
-# ax = plt.subplot(1,3,3)
-# x_samples_c = gmm.sample(10000)
-# x_samples_c = np.random.permutation(x_samples_c[0]) # need to randomly permute because gmm.sample samples 1000 from class 1, then 1000 from class 2, etc.
-# x_samples_c = generator.predict(x_samples_c)
-# canvas = getFigureOfSamplesForInput(x_samples_c, sample_dim, number_of_sample_images, grid_x, grid_y)
-# plt.imshow(canvas, cmap='Greys_r')
-# ax.set_title('Generated Images', fontsize=8)
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
+plt.figure()
 
-# plt.show()
-# # plt.savefig('images/'+ dataset_name + '_samples.png')
+ax = plt.subplot(1,3,1)
+x_samples_a = x_test
+canvas = getFigureOfSamplesForInput(x_samples_a, sample_dim, number_of_sample_images, grid_x, grid_y)
+plt.imshow(canvas, cmap='Greys_r')
+ax.set_title('Original Test Images', fontsize=8)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+ax = plt.subplot(1,3,2)
+x_samples_b = x_decoded
+canvas = getFigureOfSamplesForInput(x_samples_b, sample_dim, number_of_sample_images, grid_x, grid_y)
+plt.imshow(canvas, cmap='Greys_r')
+ax.set_title('Reconstructed Test Images', fontsize=8)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+ax = plt.subplot(1,3,3)
+x_samples_c = gmm.sample(10000)
+x_samples_c = np.random.permutation(x_samples_c[0]) # need to randomly permute because gmm.sample samples 1000 from class 1, then 1000 from class 2, etc.
+x_samples_c = generator.predict(x_samples_c)
+canvas = getFigureOfSamplesForInput(x_samples_c, sample_dim, number_of_sample_images, grid_x, grid_y)
+plt.imshow(canvas, cmap='Greys_r')
+ax.set_title('Generated Images', fontsize=8)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+plt.show()
+# plt.savefig('images/'+ dataset_name + '_samples.png')
 
 
 
